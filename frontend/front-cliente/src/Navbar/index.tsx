f752472bff6018e11ca160930281b3bc111448da
Carla-Wasovicz <Carla.almeida2@estudante.ifms.edu.br>
2021-09-20 16:28:40 -0400

    Navbar

---
+++ frontend/front-cliente/src/Navbar/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+function Navbar(){
+    return(
+        <nav className="main-navbar">
+            <a href="home" className="logo-text">PRODUTOS CASEIROS DA NANDA</a>
+        </nav>
+    )
+}
+export default Navbar;
